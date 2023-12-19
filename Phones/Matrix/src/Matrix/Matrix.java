236616a25ad9e21714a28b3076d98f74f7c34e6b
CodeWizardMagic <148693565+CodeWizardMagic@users.noreply.github.com>
2023-12-19 15:28:54 +0600

    Update Matrix.java

--- Phones/Matrix/src/Matrix/Matrix.java
+++ Phones/Matrix/src/Matrix/Matrix.java
@@ -9,15 +9,6 @@ public class Matrix {
         this.columns = columns;
         this.matrix = new double[rows][columns];
     }
-    public Matrix multiple(int a){
-       Matrix res = new Matrix(rows,columns);
-       for(int i = 0;i < rows;i++){
-           for(int j = 0;j < columns;j++){
-               res.matrix[i][j] = matrix[i][j] * a;
-           }
-       }
-    return res;
-    }
     public static void add(Matrix a, Matrix b){
         Matrix res = new Matrix(rows,columns);
         for(int i = 0;i < rows;i++){
@@ -33,6 +24,16 @@ public class Matrix {
         for(int i = 0;i < rows;i++){
             for(int j = 0;j < columns;j++){
                 res.matrix[i][j] = a.matrix[i][j] - b.matrix[i][j];
+                System.out.print(res.matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void multy(Matrix a, int b){
+        Matrix res = new Matrix(rows,columns);
+        for(int i = 0;i < rows;i++){
+            for(int j = 0;j < columns;j++){
+                res.matrix[i][j] = a.matrix[i][j] * b;
                 System.out.print(res.matrix[i][j] + " ");
             }
             System.out.println();
